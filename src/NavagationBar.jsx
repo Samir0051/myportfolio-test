4f3c8bd9984ea01dd790addfe2b70cf90f290f24
Samir <samirsanchez036@gmail.com>
2023-12-29 01:21:29 -0500

    Started Website
    yup

---
+++ src/NavagationBar.jsx
@@ -0,0 +1,22 @@
+import './NavagationBar.css'
+function NavagationBar(){
+    return(
+        <>
+        <nav>
+            <h1 id= "name">
+                Samir Sanchez Tejada
+            </h1>
+            <div>
+                <ul id ="navbar">
+                    <li><a href="index.html">Home</a> </li>
+                    <li><a href="index.html">More</a></li>
+                    <li><a href="index.html">Stuff</a></li>
+                    <li><a href="index.html">Cool</a></li>
+                </ul>
+            </div>
+        </nav>
+        </>
+    )
+}
+
+export default NavagationBar
