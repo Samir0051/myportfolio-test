2cff586761e4b55e3d90c8d77dca31db7ba579a2
Samir <samirsanchez036@gmail.com>
2023-12-29 22:06:30 -0500

    router

--- src/App.jsx
+++ src/App.jsx
@@ -8,35 +8,25 @@ import Home from "./pages/Home"
 import Cool from "./pages/Cool"
 import More from "./pages/More"
 import Stuff from "./pages/Stuff"
+import { Routes, Route, HashRouter } from "react-router-dom";
+
 
 function App() {
-  let component 
-  switch(window.location.pathname){
-    case "/myportfolio-test":
-      component = <Home />
-      break
-    case "/myportfolio-test/Home":
-      component = <Home />
-      break
-    case "/myportfolio-test/Cool":
-      component = <Cool />
-      break
-    case "/myportfolio-test/More":
-      component = <More />
-      break
-    case "/myportfolio-test/Stuff":
-      component = <Stuff />
-      break
-    default:
-      component = <Home />; // Default case, in case the path doesn't match any of the above
-  }
 
   return (
     <>
+    <HashRouter>
       <div>
         <NavagationBar></NavagationBar>
-        {component}
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/myportfolio-test/Home" element={<Home />} />
+        <Route path="/myportfolio-test/Cool" element={<Cool />} />
+        <Route path="/myportfolio-test/More" element={<More />} />
+        <Route path="/myportfolio-test/Stuff" element={<Stuff />} />
+      </Routes>
       </div>
+    </HashRouter>
     </>
   )
 }
